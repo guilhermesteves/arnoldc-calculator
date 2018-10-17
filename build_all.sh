f92e3a7b630348e7e12e64ae9d3e07a089c9e4e3
Guilherme Esteves <guilhermesteves@icloud.com>
2018-10-17 13:43:34 -0300

    add builder

---
+++ build_all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+java -jar ArnoldC.jar calculator-sum.arnoldc 
+java -jar ArnoldC.jar calculator-subtract.arnoldc 
+java -jar ArnoldC.jar calculator-multiply.arnoldc 
+java -jar ArnoldC.jar calculator-divide.arnoldc 
